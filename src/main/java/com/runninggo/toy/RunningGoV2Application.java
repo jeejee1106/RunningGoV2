6644e9747143459eb6661403b006901cb62545e0
minjee <kimmj1106@naver.com>
2023-04-10 00:04:59 +0900

    [MIGRATION] pom.xml 수정 및 Application.java 추가 1. framework관련 dependency들을 boot관련 dependency로 변경 2. 근데 왜 .idea에서 artifacts폴더가 사라졌지...? 흐음..?

---
+++ src/main/java/com/runninggo/toy/RunningGoV2Application.java
@@ -0,0 +1,13 @@
+package com.runninggo.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningGoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningGoV2Application.class, args);
+	}
+
+}
