456bef2c6130b3d9a91ded25b876c7d7e0fd3e1c
minjee <kimmj1106@naver.com>
2023-05-15 23:46:15 +0900

    [FEAT] JWT 기반 로그인 구현 중

--- src/main/java/com/runninggo/toy/auth/JwtTokenProvider.java
+++ src/main/java/com/runninggo/toy/auth/JwtTokenProvider.java
@@ -1,16 +1,106 @@
 package com.runninggo.toy.auth;
 
-import lombok.Value;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+//JWT 토큰 생성, 토큰 복호화 및 정보 추출, 토큰 유효성 검증의 기능을 구현할 클래스
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     @Value("${spring.jwt.secretKey}")
     private String secretKey;
-
     private long tokenValidTime = 1000L * 60 * 30; // 30분
+    private final UserDetailsService userDetailsService;
 
+    /**
+     * 토큰 생성 메서드
+     */
+    public String createToken(String id) { //토큰의 키가 되는 Subject를 중복되지 않는 고유한 값인 member의 id(pk)로 지정
+        Claims claims = Jwts.claims().setSubject(id);
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime)) //만료시간은 지금 시간으로부터 30분
+                .signWith(SignatureAlgorithm.HS256, secretKey) //서명할 때 사용되는 알고리즘은 HS256, 키는 위에서 지정한 값으로 진행
+                .compact();
+    }
+
+    /**
+     * 토큰으로 인증 객체(Authentication)을 얻기 위한 메소드
+     * UserDetailsService를 구현한 구현체인 MemberDetailsService에서
+     * loadUserByUsername 메소드를 구현하여 실제 DB에 저장되어 있는 회원 객체를 끌고와 인증처리를 진행
+     *
+     * 즉, 토큰을 통해 이미 인증된 객체를 memberDetailsService에서 불러와 인증객체를 얻어준다.
+     */
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getMemberId(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+
+    /**
+     * id를 얻기 위해 실제로 토큰을 디코딩하는 부분
+     * 지정한 Secret Key를 통해 서명된 JWT를 해석하여 Subject를 끌고와 리턴하여 이를 통해 인증 객체를 끌고올 수 있다.
+     */
+    public String getMemberId(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject(); //지정한 Secret Key를 통해 서명된 JWT를 해석하여 Subject를 끌고와 리턴하여 이를 통해 인증 객체를 끌고올 수 있다.
+        } catch(ExpiredJwtException e) {
+            return e.getClaims().getSubject();
+        }
+    }
+
+    /**
+     * 토큰이 만료되었는지 확인해주는 메소드
+     * token을 디코딩하여 만료시간을 끌고와 현재시간과 비교해 확인해준다.
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    // 토큰 정보를 검증하는 메서드
+    //흠 얘는 왜 에러가 나지? 위 메서드랑 비슷한 역할일 것 같다..
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+//            return true;
+//        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+//            log.info("Invalid JWT Token", e);
+//        } catch (ExpiredJwtException e) {
+//            log.info("Expired JWT Token", e);
+//        } catch (UnsupportedJwtException e) {
+//            log.info("Unsupported JWT Token", e);
+//        } catch (IllegalArgumentException e) {
+//            log.info("JWT claims string is empty.", e);
+//        }
+//        return false;
+//    }
+
+    /**
+     * 토큰은 HTTP Header에 저장되어 계속적으로 이용되어진다.
+     * 토큰을 사용하기 위해 실제로 Header에서 꺼내오는 메소드이다.
+     */
+    public String resolveToken(HttpServletRequest req) {
+        return req.getHeader("X-AUTH-TOKEN");
+    }
 }
--- src/main/java/com/runninggo/toy/config/SecurityConfig.java
+++ src/main/java/com/runninggo/toy/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.runninggo.toy.config;
 
+import com.runninggo.toy.auth.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * [스프링 시큐리티의 의존성 추가 시 일어나는 일들]
+ * - 서버가 기동되면 스프링 시큐리티의 초기화 작업 및 보안 설정이 이뤄진다.
+ * - 별도의 설정이나 구현을 하지 않아도 기본적인 웹 보안 기능이 현재 시스템에 연동된다.
+ *   1. 모든 요청은 인증이 되어야 자원에 접근이 가능하다.
+ *   2. 인증 방식은 폼 로그인 방식과 httpBasic로그인 방식을 제공한다.
+ *   3. 기본 로그인 페이지 제공
+ *   4. 기본 계정 한 개 제공 user/랜덤문자열
+ */
 
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 시큐리티 필터 체인(SpringSecurityFilterChain)에 등록됨
@@ -28,11 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { //스프링 �
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable() //rest api이므로 기본설정 미사용
-                .csrf().disable() //rest api이므로 csrf 보안 미사용. rest api를 이용한 서버라면, session 기반 인증과는 다르게 stateless하기 때문에 서버에 인증정보를 보관하지 않음..? 더 공부하기
+                .csrf().disable() //rest api이므로 csrf 보안 미사용. rest api를 이용한 서버라면, session 기반 인증과는 다르게 stateless하기 때문에 서버에 인증정보를 보관하지 않음..? 더 공부하기 ->세션, 쿠키의 로그인 방식과 JWT의 가장 큰 차이점은 서버는 클라이언트의 상태를 완전히 저장하지 않는 무상태성(Stateless)을 유지할 수 있다는 점
                 .formLogin().disable() //rest api이므로 formLogin 미사용. 보안 검증은 formLogin 방식으로 하겠다. -> 얘를 활성화 시키면 /place/recmndForm 주소를 요청했을 때 403이 아닌 로그인 페이지가 뜬다!!
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt로 인증하므로 세션 미사용
+                //여기까지가 rest-api를 위한 설정
 //                .cors()//CorsFilter라는 필터가 존재. 이를 활성화 시키는 작업.
                 .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager())) //JWT 설정을 위해서는 formLogin 기능을 빼고 이렇게 사용자가 직접 필터 클래스를 만들어줘야 한다.
                 .authorizeRequests() //여기부터 인증절차에 대한 설정을 진행하겠다. - 요청에 의한 보안검사 시작
                 .antMatchers("/").permitAll() //antMatchers : 특정 URL 에 대해서 어떻게 인증처리를 할지, permitAll : 스프링 시큐리티에서 인증이 되지 않더라도 통과
                 .antMatchers("/login/**").permitAll()
@@ -44,6 +57,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { //스프링 �
 //                .headers().frameOptions().disable();
     }
 
+    /**
+     * .httpBasic.disable()
+     * httpBasic은 기본적으로 disable이지만 켜두면 화면에 얼럿창이 뜬다.
+     * 쿠키와 세션을 이용한 방식이 아니라 request header에 id와 password값을 직접 날리는 방식이라 보안에 굉장히 취약하다.
+     * REST API에서는 오로지 토큰 방식을 이용하기 때문에 보안에 취약한 httpBasic 방식은 해제한다고 보면 된다.
+     *
+     * http.csrf().disable()
+     * CSRF를 켜두면 서버는 클라이언트 영역에 CSRF 토큰을 보낸다.
+     * 그리고 다시 클라이언트에서 서버로 CSRF 토큰이 전달되지 않으면 403 error를 뱉고 인가시켜주지 않는다.
+     * REST API에서는 CSRF 방어가 필요가 없고 더불어 CSRF 토큰을 주고 받을 필요가 없기 때문에 CSRF 설정을 해제한다.
+     *
+     * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+     * 서버를 Stateless하게 유지한다.
+     * 이걸 설정하면 Spring Security에서 세션을 만들지 않는다.
+     * 만약에 켜둔다면 JWT Token으로 로그인하더라도 클라이언트에서 Token값을 서버에 전달하지 않더라도 세션 값으로 로그인이 된다.
+     *
+     * .formLogin().disable()
+     * formLogin 기능을 끈다고 해서 form 태그 내에 로그인 기능을 못쓴다는 것은 아니다.
+     * formLogin을 끄면 초기 로그인 화면이 사라진다. 이것보다 궁극적인 이유는 JWT의 기능을 만들기 위해서다.
+     * formLogin은 세션 로그인 방식에서 로그인을 자동처리 해준다는 장점이 존재했는데, JWT에서는 로그인 방식 내에 JWT 토큰을 생성하는 로직이 필요하기 때문에 로그인 과정을 수동으로 클래스를 만들어줘야 하기 때문에 formLogin 기능을 제외한다.
+     * formLogin 기능 자체가 REST API에 반대되는 특징을 가지고 있다. formLogin의 defaultSuccessUrl 메소드로 로그인 성공 시 리다이렉트 할 주소를 입력하게 되는데 REST API에서는 서버가 페이지의 기능을 결정하면 안되기 때문에 결과적으로 필요하지 않은 formLogin은 disable한다.
+     */
+
+
     //passwordEncoder
     @Bean
     public BCryptPasswordEncoder encodePassword() {
@@ -51,13 +88,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { //스프링 �
     }
 
 }
-
-/**
- * [스프링 시큐리티의 의존성 추가 시 일어나는 일들]
- * - 서버가 기동되면 스프링 시큐리티의 초기화 작업 및 보안 설정이 이뤄진다.
- * - 별도의 설정이나 구현을 하지 않아도 기본적인 웹 보안 기능이 현재 시스템에 연동된다.
- *   1. 모든 요청은 인증이 되어야 자원에 접근이 가능하다.
- *   2. 인증 방식은 폼 로그인 방식과 httpBasic로그인 방식을 제공한다.
- *   3. 기본 로그인 페이지 제공
- *   4. 기본 계정 한 개 제공 user/랜덤문자열
- */
