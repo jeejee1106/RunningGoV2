c3dbf8652ddf07b60ff9ec90e868fbd031e7cb9a
minjee <kimmj1106@naver.com>
2023-05-26 19:52:30 +0900

    [FEAT] 스프링 시큐리티 예외 처리 중 찔끔찔끔 하니까 플로우가 잘 안그려지네.. 이번 연휴엔 빡세게..하자..

--- src/main/java/com/runninggo/toy/auth/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/runninggo/toy/auth/JwtAuthenticationEntryPoint.java
@@ -1,24 +1,45 @@
 package com.runninggo.toy.auth;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.runninggo.toy.exception.ErrorResult;
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final HandlerExceptionResolver resolver;
-
-    public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-        this.resolver = resolver;
-    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
-        resolver.resolveException(request, response, null, authException);
+        String exception = (String)request.getAttribute("exception");
+
+        ErrorResult errorResult = new ErrorResult();
+
+        if(exception == null) {
+            errorResult.setCode("[403] UNAUTHORIZED");
+            errorResult.setMessage("에러 메세지");
+        }
+        //잘못된 타입의 토큰인 경우
+        else if(exception.equals(MalformedJwtException)) {
+            errorResult.setCode("[403] UNAUTHORIZED");
+            errorResult.setMessage("에러 메세지");
+        }
+        //토큰 만료된 경우
+        else if(exception.equals(Code.EXPIRED_TOKEN.getCode())) {
+            errorResult.setCode("[403] UNAUTHORIZED");
+            errorResult.setMessage("에러 메세지");
+        }
+        //지원되지 않는 토큰인 경우
+        else if(exception.equals(Code.UNSUPPORTED_TOKEN.getCode())) {
+            errorResult.setCode("[403] UNAUTHORIZED");
+            errorResult.setMessage("에러 메세지");
+        }
+        else {
+            errorResult.setCode("[403] UNAUTHORIZED");
+            errorResult.setMessage("에러 메세지");
+        }
     }
 }
--- src/main/java/com/runninggo/toy/exception/ErrorResult.java
+++ src/main/java/com/runninggo/toy/exception/ErrorResult.java
@@ -2,9 +2,11 @@ package com.runninggo.toy.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResult {
     private String code;
     private String message;
