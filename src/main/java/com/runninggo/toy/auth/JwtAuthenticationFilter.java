1f701743aca34883e72ab2a606b7e4716fb4f52b
minjee <kimmj1106@naver.com>
2023-05-31 20:29:50 +0900

    [CHORE] 필터 공부 후 JWT관련 필터 로직 수정 어떤 로직이 더 효율적인지는 잘 모르겠음

--- src/main/java/com/runninggo/toy/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/runninggo/toy/auth/JwtAuthenticationFilter.java
@@ -1,7 +1,11 @@
 package com.runninggo.toy.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.runninggo.toy.exception.ErrorResult;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -12,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -33,15 +38,31 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         String token = resolveToken((HttpServletRequest) request);
 
         // 2. validateToken 으로 토큰 유효성 검사
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            log.info(">>>>>토큰이 유효함. 또는 토큰이 존재함");
-            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            //SecurityContext 에 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                log.info(">>>>>토큰이 유효함. 또는 토큰이 존재함");
+                // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                //SecurityContext 에 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (JwtException ex) {
+            log.info(">>>>>토큰이 유효하지 않음.");
+            // JwtAuthenticationFilter에서 예외 발생하면 바로 setErrorResponse 호출
+            setErrorResponse((HttpServletRequest) request, (HttpServletResponse) response, ex);
+        } finally {
+            chain.doFilter(request, response);
         }
+//        if (token != null && jwtTokenProvider.validateToken(token)) {
+//            log.info(">>>>>토큰이 유효함. 또는 토큰이 존재함");
+//            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서
+//            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+//            //SecurityContext 에 저장
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        } else {
+//
+//        }
         //doFilter 메소드를 통해 다음 필터로 넘어가고, 실제 AuthenticationFilter에서 이미 인증되어 있는 객체를 통해 인증이 되게 된다.
-        chain.doFilter(request, response);
     }
 
     // Request Header 에서 토큰 정보 추출
@@ -53,4 +74,22 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         return null;
     }
 
+    public void setErrorResponse(HttpServletRequest req, HttpServletResponse res, Throwable ex) throws IOException {
+
+        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        final ObjectMapper mapper = new ObjectMapper();
+        ErrorResult errorResult = new ErrorResult("401", ex.getMessage());
+
+//        final Map<String, Object> body = new HashMap<>();
+//        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+//        body.put("error", "Unauthorized");
+        // ex.getMessage() 에는 jwtException을 발생시키면서 입력한 메세지가 들어있다.
+//        body.put("message", ex.getMessage());
+//        body.put("path", req.getServletPath());
+
+//        mapper.writeValue(res.getOutputStream(), body);
+        mapper.writeValue(res.getOutputStream(), errorResult);
+        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED); //현재 200OK를 반환함.. 상태코드를 400으로 낼 순 없을까?
+    }
 }
--- src/main/java/com/runninggo/toy/auth/JwtExceptionFilter.java
+++ src/main/java/com/runninggo/toy/auth/JwtExceptionFilter.java
@@ -12,8 +12,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
@@ -21,6 +19,12 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
     /*
     인증 오류가 아닌, JWT 관련 오류는 이 필터에서 따로 잡아낸다.
     이를 통해 JWT 만료 에러와 인증 에러를 따로 잡아낼 수 있다.
+
+    라고 해서 어떤 블로그에서 작성한 코드를 그대로 가져온거긴한데....
+    필터에 대해 다시 공부하고 보니 굳이 이렇게 따로 구현하지 않고 JwtAuthenticationFilter에서 구현해도 되지 않을까 싶어 코드 옮김..
+    어떤 방식이 더 효율적인지...?
+
+    어쨋든 230531 현재 해당 필터는 사용하지 않음!
      */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
@@ -36,14 +40,16 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
 
         res.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-//        final Map<String, Object> body = new HashMap<>();
+        final ObjectMapper mapper = new ObjectMapper();
         ErrorResult errorResult = new ErrorResult("401", ex.getMessage());
+
+//        final Map<String, Object> body = new HashMap<>();
 //        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
 //        body.put("error", "Unauthorized");
         // ex.getMessage() 에는 jwtException을 발생시키면서 입력한 메세지가 들어있다.
 //        body.put("message", ex.getMessage());
 //        body.put("path", req.getServletPath());
-        final ObjectMapper mapper = new ObjectMapper();
+
 //        mapper.writeValue(res.getOutputStream(), body);
         mapper.writeValue(res.getOutputStream(), errorResult);
         res.setStatus(HttpServletResponse.SC_UNAUTHORIZED); //현재 200OK를 반환함.. 상태코드를 400으로 낼 순 없을까?
